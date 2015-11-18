27e5f726868229d308ca7d2ff8e0a6b6ab0fbff7
Chris Kwok <chris.kwok@climate.com>
2015-11-17 23:51:16 -0800

    run rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151116074719) do
+ActiveRecord::Schema.define(version: 20151118064552) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,4 +24,51 @@ ActiveRecord::Schema.define(version: 20151116074719) do
     t.datetime "updated_at",              null: false
   end
 
+  create_table "parking_business_hours", force: :cascade do |t|
+    t.integer  "hour_type",      default: 0
+    t.string   "from_to"
+    t.integer  "parking_lot_id"
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
+
+  add_index "parking_business_hours", ["parking_lot_id"], name: "index_parking_business_hours_on_parking_lot_id", using: :btree
+
+  create_table "parking_lots", force: :cascade do |t|
+    t.integer  "objectid"
+    t.integer  "buslic_location_id"
+    t.string   "dea_facility_address"
+    t.integer  "dea_stalls"
+    t.string   "fac_name"
+    t.integer  "disabled"
+    t.string   "op_name"
+    t.string   "op_phone"
+    t.string   "op_phone2"
+    t.string   "op_web"
+    t.integer  "payment_type",                                   default: 0
+    t.integer  "other",                                          default: 0
+    t.string   "webname"
+    t.integer  "regionid"
+    t.integer  "outofserv",                                      default: 0
+    t.integer  "vacant"
+    t.string   "signid"
+    t.decimal  "longtitude",           precision: 15, scale: 13
+    t.decimal  "latitude",             precision: 15, scale: 13
+    t.datetime "created_at",                                                 null: false
+    t.datetime "updated_at",                                                 null: false
+  end
+
+  create_table "parking_rates", force: :cascade do |t|
+    t.integer  "rate_type",      default: 0
+    t.decimal  "price"
+    t.string   "description"
+    t.integer  "parking_lot_id"
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
+
+  add_index "parking_rates", ["parking_lot_id"], name: "index_parking_rates_on_parking_lot_id", using: :btree
+
+  add_foreign_key "parking_business_hours", "parking_lots"
+  add_foreign_key "parking_rates", "parking_lots"
 end
