358d48e583367ac73a1533a11842588d52416ea0
Chris Kwok <chris.kwok@climate.com>
2015-11-17 23:30:52 -0800

    update db migration for createParkingBusinessHours

--- db/migrate/20151118064552_create_parking_business_hours.rb
+++ db/migrate/20151118064552_create_parking_business_hours.rb
@@ -2,6 +2,8 @@ class CreateParkingBusinessHours < ActiveRecord::Migration
   def change
     create_table :parking_business_hours do |t|
 
+	  t.column :hour_type, :integer, default: 0 # monfri, sun, sat
+      t.string :from_to
       t.timestamps null: false
     end
   end
