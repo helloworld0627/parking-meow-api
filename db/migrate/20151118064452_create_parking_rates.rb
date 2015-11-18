9b223111aadb6e56603842158c1590f87bc35098
Chris Kwok <chris.kwok@climate.com>
2015-11-17 23:20:41 -0800

    update db migration for createParkingRates

--- db/migrate/20151118064452_create_parking_rates.rb
+++ db/migrate/20151118064452_create_parking_rates.rb
@@ -2,6 +2,10 @@ class CreateParkingRates < ActiveRecord::Migration
   def change
     create_table :parking_rates do |t|
 
+      t.column :rate_type, :integer, default: 0 # 1hr, 2hr ,3hr, all_day
+      t.decimal :price
+      t.string :description
+
       t.timestamps null: false
     end
   end
