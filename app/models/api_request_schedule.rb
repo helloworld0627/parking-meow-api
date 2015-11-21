e49153f2adba91fc6884ddce6696cb16960ed914
Chris Kwok <chris.kwok@climate.com>
2015-11-21 11:55:58 -0800

    adding logic to scheduler

--- app/models/api_request_schedule.rb
+++ app/models/api_request_schedule.rb
@@ -1,5 +1,5 @@
 class ApiRequestSchedule < ActiveRecord::Base
-	enum status: [ :new, :run, :succeed, :fail ]
+	enum status: [ :prepare, :run, :succeed, :fail ]
 
 	@@api_path = 'https://data.seattle.gov/resource/3neb-8edu.json'
 
@@ -19,6 +19,7 @@ class ApiRequestSchedule < ActiveRecord::Base
 
 	def self.parse_JSON_to_Obj(parsed_json)
 		parsed_json.each do |item|
+			# parse parking lot
 			parkingLot = ParkingLot.new do |p|
 				p.objectid = item["objectid"]
 				p.buslic_location_id = item["buslic_location_id"]
@@ -30,20 +31,34 @@ class ApiRequestSchedule < ActiveRecord::Base
      			p.op_phone = item["op_phone"]
      			p.op_phone2 = item["op_phone2"]
     		    p.op_web = item["op_web"]
-  			    #p.payment_type, :integer, default: 0
- 	   		    #t.column :other, :integer, default: 0 # Y/N text
+  			    p.payment_type = item["payment_type"]
+ 	   		    p.other = item["op_web"]
 			    p.webname = item["webname"]
  		        p.regionid = item["regionid"]
- 		        #t.column :outofserv, :integer, default: 0 #Y/N text
+ 		        p.outofserv = item["outofserv"]
     		    p.vacant = item["vacant"]
 		        p.signid = item["signid"]
+		        #shape
 		        shape = item["shape"]
   		        p.longtitude= shape["longitude"]
    		        p.latitude = shape["latitude"]
    		        #puts p.to_json
 			end
 
-			rte = 
+			# parse parking rate
+			["rte_1hr", "rte_2hr", "rte_3hr", "rte_allday"].each do |rate_type|
+				rate = ParkingRate.new do |r|
+					puts rate_type
+					if item.key? rate_type
+						r.rate_type = rate_type
+						r.price = item[rate_type]
+						# puts r.to_json
+					end
+				end
+			end
+
+			# parse biz hour
+
 		end
 	end	
 end
